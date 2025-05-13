83abbb6760c8289c91ebcc35e1250e9ca9771390
TinashG7 <tinashegatimu@gmail.com>
2025-05-13 16:25:36 +0300

    class work

---
+++ input_output.py
@@ -0,0 +1,22 @@
+#name = str(input("What is your name? "))
+
+#number = int(input("What is your number? "))
+
+#print(f"The name is {name} and phone number is {number}")
+
+#print("The name is " + name + " and phone number is " + str(number))
+
+#print("The name is %s and phone number is %i "%(name, number))
+
+#print("The name is {} and phone number is {}".format(*args:name, number))
+
+#names = ["a","b", "c", ]
+
+print("Yo")
+
+def main():
+    print("Inside the function")
+    num = 13
+    num2 = 78
+    if num > num2:
+        print("Sucker")
